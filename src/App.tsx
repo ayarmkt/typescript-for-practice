ba93bf9698619db0b2c83dad9ece2a2f0c889a78
ayarmkt <ayat.0906@gmail.com>
2022-03-09 10:53:11 +0100

    finished course

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Counter } from './Counter';
 import { TextField } from './TextField';
 
 const App: React.FC = () => {
   return (
     <div>
-      <TextField text='hello' person={{ firstName: '', lastName: '' }} />
+      {/* <TextField text='hello' person={{ firstName: '', lastName: '' }} /> */}
+      <Counter>
+        {({ count, setCount }) => (
+          <div>
+            {count}
+            <button onClick={() => setCount(count + 1)}>+</button>
+          </div>
+        )}
+      </Counter>
     </div>
   );
 };
--- src/TextField.tsx
+++ src/TextField.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 interface Person {
   firstName: string;
@@ -15,22 +14,25 @@ interface Props {
     f1: string;
   };
   person: Person;
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 interface TextNode {
   text: string;
 }
 
-export const TextField: React.FC<Props> = () => {
+export const TextField: React.FC<Props> = ({ handleChange }) => {
   //const [count, setCount] = useState<number | null>(5);
   //const [count, setCount] = useState<{ text: string }>({ text: 'hello' });
-  const [count, setCount] = useState<TextNode>({ text: 'hello' });
+  //   const [count, setCount] = useState<TextNode>({ text: 'hello' });
+  //   setCount({ text: 'bye' });
 
-  setCount({ text: 'bye' });
+  const inputRef = useRef<HTMLInputElement>(null);
+  const divRef = useRef<HTMLDivElement>(null);
 
   return (
-    <div>
-      <input />
+    <div ref={divRef}>
+      <input ref={inputRef} onChange={handleChange} />
     </div>
   );
 };
